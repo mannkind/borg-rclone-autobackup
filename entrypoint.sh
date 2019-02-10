7aa1b1b4bf67b42209ad282fe40f18ceaa5635d0
Gordy F <gordyf@gordyf.com>
2019-02-09 19:04:59 -0800

    Add Google Cloud Storage support

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ if [[ $B2_ID && $B2_KEY ]]; then
     echo -e "[b2]\ntype = b2\naccount = $B2_ID\nkey = $B2_KEY\nendpoint = \n" > /root/.rclone.conf
 fi
 
+# Configure RClone for GCS
+if [[ $GCS_PROJECT_NUMBER ]]; then
+    echo -e "[gcs]\ntype = google cloud storage\nclient_id =\nclient_secret =\nservice_account_file = /google-service-account.json\nproject_number = $GCS_PROJECT_NUMBER\nobject_acl = private\nbucket_acl = private\N" > /root/.rclone.conf
+fi
+
 # Schedule Backup
 echo "$BACKUP_SCHEDULE /app/backup.sh" > /var/spool/cron/crontabs/root
 
